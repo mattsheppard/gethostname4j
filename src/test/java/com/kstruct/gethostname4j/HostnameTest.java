ff7c50e90bc38a4d675555a6edfbb1a3cb905e02
Matthew Sheppard <matthew.sheppard@gmail.com>
2017-03-07 19:16:26 +1100

    Downgrade junit to support java 1.4

--- src/test/java/com/kstruct/gethostname4j/HostnameTest.java
+++ src/test/java/com/kstruct/gethostname4j/HostnameTest.java
@@ -1,9 +1,8 @@
 package com.kstruct.gethostname4j;
 
-import org.junit.Test;
+import junit.framework.TestCase;
 
-public class HostnameTest {
-    @Test
+public class HostnameTest extends TestCase {
     public void test() {
         String hostname = Hostname.getHostname();
         System.out.println("Dear human - does this look like the right hostname? " + hostname);
